80bf7c9c13cecc607bd246d5db1573f66fa1d3c6
Silviaillescas <silviaillesca402@gmail.com>
2023-11-22 16:03:31 -0600

    barra

--- app/src/main/java/silvia/illescas/proy1/Parqueoasignado.kt
+++ app/src/main/java/silvia/illescas/proy1/Parqueoasignado.kt
@@ -1,40 +1,65 @@
 package silvia.illescas.proy1
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Place
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import silvia.illescas.proy1.ui.theme.Proy1Theme
+import kotlin.random.Random
 
-class Parqueoasignado: ComponentActivity() {
+class ParqueoAsignado : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Proy1Theme() {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier,
-                    color = MaterialTheme.colorScheme.background,
-                ) {
-                    ParqueoScreen()
-                }
+                Scaffold(
+                    bottomBar = {
+                        BottomAppBar(
+                            modifier = Modifier.background(Color.Green)
+                        ) {
+                            BottomAppIcon(Icons.Default.Home, stringResource(R.string.home))
+                            BottomAppIcon(Icons.Default.Place, stringResource(R.string.place))
+                            BottomAppIcon(Icons.Default.AccountCircle, stringResource(R.string.profile))
+                            BottomAppIcon(Icons.Default.Info, stringResource(R.string.help))
+                        }
+                    },
+                    content = {
+                        ParqueoScreen()
+                    }
+                )
             }
         }
     }
 }
+
+private fun RowScope.BottomAppIcon(icon: ImageVector, contentDescription: String) {
+
+}
+
 @Composable
 fun ParqueoScreen() {
-    var parqueoText by remember { mutableStateOf("E09") }
+    val random = Random(System.currentTimeMillis())
+    var parqueoText by remember { mutableStateOf("E" + random.nextInt(100)) }
+
 
     Box(
         modifier = Modifier.fillMaxSize().background(Color(0xFF4CAF50)), // Fondo verde
