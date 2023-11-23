a03c63f0cfb5e7e71a09c741943f352f26046730
DannyEst6109 <72481675+DannyEst6109@users.noreply.github.com>
2023-11-22 21:35:07 -0600

    pantallas

---
+++ app/src/main/java/com/example/proyectov50/ui/theme/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.proyectov50.ui.theme
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ParqueoScreen()
+        }
+    }
+}
