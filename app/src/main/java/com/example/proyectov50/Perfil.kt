49c0a80f007b79f5935c2eb26f1fc900bf6d46c6
michimejia <mejiavillelamichelle@gmail.com>
2023-11-22 21:49:49 -0600

    Perfil actualizado

---
+++ app/src/main/java/com/example/proyectov50/Perfil.kt
@@ -0,0 +1,150 @@
+package com.example.proyectov50
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.sharp.Face
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.example.proyectov50.ui.theme.ProyectoV50Theme
+
+class Perfil : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ProyectoV50Theme{
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.White
+                ) {
+                    ProfileFun()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ProfileFun(modifier: Modifier = Modifier) {
+    Surface(color = Color.White) {
+        Column(
+            modifier = modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            // Green box on top
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(240.dp)
+                    .background(Color.Green)
+            ) {
+                    Image(
+                        imageVector = Icons.Filled.AccountCircle,
+                        contentDescription = "Profile Picture",
+                        modifier = Modifier.size(120.dp)
+                    )
+                }
+
+
+
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "John Doe",
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                modifier = Modifier.padding(top = 16.dp)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Profile information
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Spacer(modifier = Modifier.height(16.dp))
+
+                ProfileCard(
+                    icon = Icons.Sharp.Face,
+                    title = "Personal Information",
+                    items = listOf(
+                        "Carrera: Ciencias de la Computación",
+                        "Username: johndoe123",
+                        "Carnet: 22596"
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                ProfileCard(
+                    icon = Icons.Filled.ShoppingCart,
+                    title = "Vehicle Information",
+                    items = listOf(
+                        "Model: Sedan",
+                        "Plate: ABC123"
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+        }
+    }
+}
+
+@Composable
+fun ProfileCard(
+    icon: ImageVector,
+    title: String,
+    items: List<String>
+) {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    imageVector = icon,
+                    contentDescription = "Icon",
+                    modifier = Modifier.size(20.dp)
+                )
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                Column {
+                    Text(text = title, fontWeight = FontWeight.Bold)
+
+                    items.forEach { item ->
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Text(text = item)
+                    }
+                }
+            }
+        }
+    }
+}
